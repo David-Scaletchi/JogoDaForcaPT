9e2fd1a23ae9d0617c4177d3e6eeecfb835f8d4d
David Scaletchi <163128101+David-Scaletchi@users.noreply.github.com>
2024-03-13 10:34:18 +0000

    Update JogoDaForca.py

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -9,20 +9,23 @@ palavras2 = ["abacate", "laranja", "banana", "pessego", "tomate"]
 def escolherPalavra1():
     return random.choice(palavras)
 
+# Aqui uso o random para sortear as palavras com tema2
 def escolherPalavra2():
     return random.choice(palavras2)
 
 
-
+#Função para jogar o jogo se for o tema1
 def jogarForca():
     palavraTema1 = escolherPalavra1()
     letrasCorretas = set()
     tentativasMaximas = 6
     tentativas = 0
 
+    #Aqui peço ao usuário uma palavra e adiciono a lista com o append()
     novaPalavra = input("Digite uma nova palavra para dicionar com o tema (animais): ")
     palavras.append(novaPalavra)
 
+    #Aqui criei um while para o jogo continuar até chegar ao numero de tentativas máximas
     while tentativas < tentativasMaximas:
                     
 
@@ -52,18 +55,21 @@ def jogarForca():
                 print(f"Parabéns! Acertaste pro player: {palavraTema1}")
                 return
 
-
+    #Se chegar ao numero de tentativas máximas faz este print
     print(f"\033[31mJá atingiste o numero maximo de erros, a palavra era:\033[m \033[32m{palavraTema1}\033[m")
 
+#Aqui é a função para jogar com o tema 2
 def jogarForca2():
     palavraTema2 = escolherPalavra2()
     letrasCorretas = set()
     tentativasMaximas = 6
     tentativas = 0
 
+    #Aqui peço ao usuário uma palavra e adiciono a lista com o append()
     novaPalavra = input("Digite uma nova palavra para dicionar com o tema (frutas): ")
     palavras2.append(novaPalavra)
 
+    #Aqui criei um while para o jogo continuar até chegar ao numero de tentativas máximas
     while tentativas < tentativasMaximas:
                     
     
@@ -94,24 +100,28 @@ def jogarForca2():
                 print(f"Parabéns! Acertaste pro player: {palavraTema2}")
                 return
 
-         
+    #Se chegar ao numero de tentativas máximas faz este print    
     print(f"\033[31mJá atingiste o numero maximo de erros, a palavra era:\033[m \033[32m {palavraTema2}\033[m")
 
+#Aqui é a função main, faz o usuário escolher o tema
 def main():
     print("1-animais")
     print("2-frutas")
     tema:int = int(input("Escolhe um tema: "))
 
+    #Se o tema for 1 executa a função jogarForca()
     if tema == 1:
         jogarForca()
 
+    #Se o tema for 1 executa a função jogarForca()
     elif tema == 2:
         jogarForca2()
 
+    #Aqui é uma verificação se for um numero sem tema
     else:
         print("METE UM NUMERO QUE TEJA UM TEMA ASSOCIADO")
 
-
+#Aqui é o main em si
 if __name__ == "__main__":
     print("JOGO DA FORCA")
     print("")
